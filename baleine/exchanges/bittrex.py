105f9d499c598dccf274897126fd9cac157a24e1
Julien Hartmann <julien@etherdream.org>
2018-01-13 04:35:46 +0000

    Dynamically fetch bittrex pairs from API

--- baleine/exchanges/bittrex.py
+++ baleine/exchanges/bittrex.py
@@ -1,88 +1,34 @@
 import aiohttp
 import pandas
+import time
 from baleine import exchange
 
 
 @exchange.register
 class BittrexExchange(exchange.Exchange):
     name = 'bittrex'
-    pairs = (   # TODO: replace with dynamic query to exchange
-        ("LTC", "BTC"), ("DOGE", "BTC"), ("VTC", "BTC"), ("PPC", "BTC"),
-        ("FTC", "BTC"), ("RDD", "BTC"), ("NXT", "BTC"), ("DASH", "BTC"),
-        ("POT", "BTC"), ("BLK", "BTC"), ("EMC2", "BTC"), ("XMY", "BTC"),
-        ("AUR", "BTC"), ("EFL", "BTC"), ("GLD", "BTC"), ("SLR", "BTC"),
-        ("PTC", "BTC"), ("GRS", "BTC"), ("NLG", "BTC"), ("RBY", "BTC"),
-        ("XWC", "BTC"), ("MONA", "BTC"), ("THC", "BTC"), ("ENRG", "BTC"),
-        ("ERC", "BTC"), ("VRC", "BTC"), ("CURE", "BTC"), ("XMR", "BTC"),
-        ("CLOAK", "BTC"), ("START", "BTC"), ("KORE", "BTC"), ("XDN", "BTC"),
-        ("TRUST", "BTC"), ("NAV", "BTC"), ("XST", "BTC"), ("BTCD", "BTC"),
-        ("VIA", "BTC"), ("PINK", "BTC"), ("IOC", "BTC"), ("CANN", "BTC"),
-        ("SYS", "BTC"), ("NEOS", "BTC"), ("DGB", "BTC"), ("BURST", "BTC"),
-        ("EXCL", "BTC"), ("SWIFT", "BTC"), ("DOPE", "BTC"), ("BLOCK", "BTC"),
-        ("ABY", "BTC"), ("BYC", "BTC"), ("XMG", "BTC"), ("BLITZ", "BTC"),
-        ("BAY", "BTC"), ("FAIR", "BTC"), ("SPR", "BTC"), ("VTR", "BTC"),
-        ("XRP", "BTC"), ("GAME", "BTC"), ("COVAL", "BTC"), ("NXS", "BTC"),
-        ("XCP", "BTC"), ("BITB", "BTC"), ("GEO", "BTC"), ("FLDC", "BTC"),
-        ("GRC", "BTC"), ("FLO", "BTC"), ("NBT", "BTC"), ("MUE", "BTC"),
-        ("XEM", "BTC"), ("CLAM", "BTC"), ("DMD", "BTC"), ("GAM", "BTC"),
-        ("SPHR", "BTC"), ("OK", "BTC"), ("SNRG", "BTC"), ("PKB", "BTC"),
-        ("CPC", "BTC"), ("AEON", "BTC"), ("ETH", "BTC"), ("GCR", "BTC"),
-        ("TX", "BTC"), ("BCY", "BTC"), ("EXP", "BTC"), ("INFX", "BTC"),
-        ("OMNI", "BTC"), ("AMP", "BTC"), ("AGRS", "BTC"), ("XLM", "BTC"),
-        ("BTC", "USDT"), ("CLUB", "BTC"), ("VOX", "BTC"), ("EMC", "BTC"),
-        ("FCT", "BTC"), ("MAID", "BTC"), ("EGC", "BTC"), ("SLS", "BTC"),
-        ("RADS", "BTC"), ("DCR", "BTC"), ("BSD", "BTC"), ("XVG", "BTC"),
-        ("PIVX", "BTC"), ("XVC", "BTC"), ("MEME", "BTC"), ("STEEM", "BTC"),
-        ("2GIVE", "BTC"), ("LSK", "BTC"), ("PDC", "BTC"), ("BRK", "BTC"),
-        ("DGD", "BTC"), ("DGD", "ETH"), ("WAVES", "BTC"), ("RISE", "BTC"),
-        ("LBC", "BTC"), ("SBD", "BTC"), ("BRX", "BTC"), ("ETC", "BTC"),
-        ("ETC", "ETH"), ("STRAT", "BTC"), ("UNB", "BTC"), ("SYNX", "BTC"),
-        ("TRIG", "BTC"), ("EBST", "BTC"), ("VRM", "BTC"), ("SEQ", "BTC"),
-        ("REP", "BTC"), ("SHIFT", "BTC"), ("ARDR", "BTC"), ("XZC", "BTC"),
-        ("NEO", "BTC"), ("ZEC", "BTC"), ("ZCL", "BTC"), ("IOP", "BTC"),
-        ("GOLOS", "BTC"), ("UBQ", "BTC"), ("KMD", "BTC"), ("GBG", "BTC"),
-        ("SIB", "BTC"), ("ION", "BTC"), ("LMC", "BTC"), ("QWARK", "BTC"),
-        ("CRW", "BTC"), ("SWT", "BTC"), ("MLN", "BTC"), ("ARK", "BTC"),
-        ("DYN", "BTC"), ("TKS", "BTC"), ("MUSIC", "BTC"), ("DTB", "BTC"),
-        ("INCNT", "BTC"), ("GBYTE", "BTC"), ("GNT", "BTC"), ("NXC", "BTC"),
-        ("EDG", "BTC"), ("LGD", "BTC"), ("TRST", "BTC"), ("GNT", "ETH"),
-        ("REP", "ETH"), ("ETH", "USDT"), ("WINGS", "ETH"), ("WINGS", "BTC"),
-        ("RLC", "BTC"), ("GNO", "BTC"), ("GUP", "BTC"), ("LUN", "BTC"),
-        ("GUP", "ETH"), ("RLC", "ETH"), ("LUN", "ETH"), ("GNO", "ETH"),
-        ("APX", "BTC"), ("HMQ", "BTC"), ("HMQ", "ETH"), ("ANT", "BTC"),
-        ("TRST", "ETH"), ("ANT", "ETH"), ("SC", "BTC"), ("BAT", "ETH"),
-        ("BAT", "BTC"), ("ZEN", "BTC"), ("1ST", "BTC"), ("QRL", "BTC"),
-        ("1ST", "ETH"), ("QRL", "ETH"), ("CRB", "BTC"), ("CRB", "ETH"),
-        ("LGD", "ETH"), ("PTOY", "BTC"), ("PTOY", "ETH"), ("MYST", "BTC"),
-        ("MYST", "ETH"), ("CFI", "BTC"), ("CFI", "ETH"), ("BNT", "BTC"),
-        ("BNT", "ETH"), ("NMR", "BTC"), ("NMR", "ETH"), ("LTC", "ETH"),
-        ("XRP", "ETH"), ("SNT", "BTC"), ("SNT", "ETH"), ("DCT", "BTC"),
-        ("XEL", "BTC"), ("MCO", "BTC"), ("MCO", "ETH"), ("ADT", "BTC"),
-        ("ADT", "ETH"), ("FUN", "BTC"), ("FUN", "ETH"), ("PAY", "BTC"),
-        ("PAY", "ETH"), ("MTL", "BTC"), ("MTL", "ETH"), ("STORJ", "BTC"),
-        ("STORJ", "ETH"), ("ADX", "BTC"), ("ADX", "ETH"), ("DASH", "ETH"),
-        ("SC", "ETH"), ("ZEC", "ETH"), ("ZEC", "USDT"), ("LTC", "USDT"),
-        ("ETC", "USDT"), ("XRP", "USDT"), ("OMG", "BTC"), ("OMG", "ETH"),
-        ("CVC", "BTC"), ("CVC", "ETH"), ("PART", "BTC"), ("QTUM", "BTC"),
-        ("QTUM", "ETH"), ("XMR", "ETH"), ("XEM", "ETH"), ("XLM", "ETH"),
-        ("NEO", "ETH"), ("XMR", "USDT"), ("DASH", "USDT"), ("BCC", "ETH"),
-        ("BCC", "USDT"), ("BCC", "BTC"), ("DNT", "BTC"), ("DNT", "ETH"),
-        ("NEO", "USDT"), ("WAVES", "ETH"), ("STRAT", "ETH"), ("DGB", "ETH"),
-        ("FCT", "ETH"), ("OMG", "USDT"), ("ADA", "BTC"), ("MANA", "BTC"),
-        ("MANA", "ETH"), ("SALT", "BTC"), ("SALT", "ETH"), ("TIX", "BTC"),
-        ("TIX", "ETH"), ("RCN", "BTC"), ("RCN", "ETH"), ("VIB", "BTC"),
-        ("VIB", "ETH"), ("MER", "BTC"), ("POWR", "BTC"), ("POWR", "ETH"),
-        ("BTG", "BTC"), ("BTG", "ETH"), ("BTG", "USDT"), ("ADA", "ETH"),
-        ("ENG", "BTC"), ("ENG", "ETH"), ("ADA", "USDT"), ("XVG", "USDT"),
-        ("NXT", "USDT"), ("UKG", "BTC"), ("UKG", "ETH"),
-    )
 
     API = 'https://bittrex.com/api/v1.1'
+    PAIR_URL = '%s/public/getmarkets' % API
     BOOK_URL = '%s/public/getorderbook?market={tickers[1]}-{tickers[0]}&type=both' % API
     TICKER_URL = '%s/public/getmarketsummary?market={tickers[1]}-{tickers[0]}' % API
 
     async def get_pairs(self):
-        return self.pairs
+        timestamp = getattr(self, '_pairs_timestamp', None)
+        if timestamp is None or timestamp + 3600 < time.time():
+            response = await aiohttp.request('GET', self.PAIR_URL)
+            data = await response.json()
+            if not data.get('success', False):
+                raise IOError('Request failed: %s' % data['message'])
+
+            self._pairs = pairs = set(
+                (item['MarketCurrency'], item['BaseCurrency'])
+                for item in data['result']
+            )
+            self._pairs_timestamp = time.time()
+        else:
+            pairs = self._pairs
+        return pairs
 
     async def get_order_book(self, pair, depth):
         """ Return a 2-tuple of (bid, ask) data frames """
